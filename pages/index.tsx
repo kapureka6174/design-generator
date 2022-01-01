52532b67d94e8d546dcf3275c00dcd62eafb3006
kapureka6174 <mitsu.n.0109@gmail.com>
2022-01-01 19:23:34 +0900

    [update] tailwind CSSの適用の確認

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>こんにちは</h1>
+      <h1 className="text-2xl">こんにちは</h1>
     </div>
   );
 };
